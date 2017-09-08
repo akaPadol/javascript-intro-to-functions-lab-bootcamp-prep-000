e016b34dabf12cd93cfde226700f553151373b45
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-08 17:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if(string.toLowerCase===string){
     return "I can't ear you!"
   }else if(string.ToUpperCase===string){
-    return "YES INDEED"
+    return "YES INDEED!"
   }else if(string==="I love you, Grandma."){
     return "I love you, too."
   }
