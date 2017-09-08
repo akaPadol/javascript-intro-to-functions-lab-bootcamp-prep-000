7c5d6bfd31e1688194a8d2cdfef3932056f6b66c
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-08 16:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.ToUpperCase);
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase===string){
+    return "I can't ear you!"
+  }else if(string.ToUpperCase===string){
+    return "YES INDEED"
+  }else if(string==="I love you, Grandma.")
+}
